ed0852716b3abd01d66f8e4cb390a4ef7b5df857
lenglee <lengleevaja@gmail.com>
2023-06-15 12:15:40 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import {auth, provider} from './FirebaseConfig';
+import axios from 'axios'
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  const [profilePicture, setProfilePicture] = useState(null);
+
+  const handleFacebookLogin=()=>{
+    signInWithPopup(auth, provider).then((result)=>{
+      setUser(result.user);
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      const credential = FacebookAuthProvider.credentialFromResult(result);
+      const accessToken = credential.accessToken;
+      // fetch facebook graph api to get user actual profile picture
+      fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/picture?type=large&access_token=${accessToken}`)
+      .then((response)=>response.blob())
+      .then((blob)=>{
+        setProfilePicture(URL.createObjectURL(blob));
+      })
+      const user = result.user;
+
+        if (user) {
+          user
+            .getIdToken()
+            .then((idToken) => {
+              const url = 'http://localhost:8080/home';
+              const headers = { 'Content-Type': 'application/json' };
+              const data = {
+                query: `
+                  mutation Mutation($data: IdToken) {
+                    signInWithFacebook(data: $data) {
+                      accessToken
+                    }
+                  }
+                `,
+                variables: {
+                  data: {
+                    idToken: idToken,
+                  },
+                },
+              };
+
+              axios
+                .post(url, data, { headers })
+                .then((response) => {
+                  console.log(response);
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  const handleLogout=()=>{
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='box'>
+        {user?(
+              <>
+                <button className='btn btn-secondary btn-md'
+                  onClick={handleLogout}>
+                  LOGOUT
+                </button>
+                <h3>Welcome {user.displayName}</h3>
+                <p>{user.email}</p>
+                <div className='photo'>
+                  <img src={profilePicture} alt="dp" referrerPolicy='no-referrer'/>
+                </div>
+              </>
+            ):(
+              <button className="btn btn-primary btn-md"
+                onClick={handleFacebookLogin}>
+                  Sign In With Facebook
+              </button>
+           )} 
+      </div>
     </div>
   );
 }
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { FacebookAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCmuJ9T_b0Mfwe950gsYSy1pnzV2GXm4PE",
+    authDomain: "auth-facebook-54693.firebaseapp.com",
+    projectId: "auth-facebook-54693",
+    storageBucket: "auth-facebook-54693.appspot.com",
+    messagingSenderId: "874856560243",
+    appId: "1:874856560243:web:8abb1737ce8cc058609c06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new FacebookAuthProvider();
+provider.addScope("email"); // Add the 'email' scope to retrieve the user's email
+//provider.addScope("user_phone_number"); // Add the 'user_phone_number' scope to retrieve the user's phone number
+
+export { auth, provider }
